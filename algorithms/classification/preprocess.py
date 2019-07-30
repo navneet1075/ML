8dc077c6cea18e0caead81421620ddba9b5ef8e4
Navneet Pathak <navneet1075@gmail.com>
2019-07-30 10:06:02 +0800

    code cleanup and segregating the responsibility

--- algorithms/classification/preprocess.py
+++ algorithms/classification/preprocess.py
@@ -4,67 +4,38 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_selection import RFE
 
-from classification.train_and_evaluate import train_and_evaluate, predict_actual
-
-
 def preprocess_train_data():
     train_data = loaddata.load_train_data("./data/titanic-dataset","train.csv")
-
-
     print(' the number of samples in the training data is {}', train_data.shape[0])
 
-
     # applying imputation to age by using median
-
     train_df = train_data.copy()
     train_df['Age'].fillna(train_data["Age"].median(skipna=True),inplace=True)
 
-
-    # if embarked is missing for a given row then apply the port of most embarkment in that row
-
+    # if embarked is missing for a given row then apply the port of most embarkation in that row
     train_df['Embarked'].fillna(train_data['Embarked'].value_counts().idxmax(),inplace=True)
 
-
     # dropping cabin column as most of the values are null
-
     train_df.drop('Cabin', axis=1, inplace=True)
 
-    # check missing values in the adjussted data
-
+    # check missing values in the adjusted data
     print('adjusted misssing value', train_df.isna().sum())
 
-
     # create additional variables
-
     train_df['TravelAlone'] = np.where(train_df['SibSp']+ train_df['Parch']>0 , 0,1)
-
-
     train_df.drop('SibSp', axis=1, inplace=True)
-
     train_df.drop('Parch', axis=1, inplace=True)
 
     # create categorical variables
-
     training = pd.get_dummies(train_df,columns=['Pclass','Embarked','Sex'])
-
     training.drop("Sex_female", axis=1, inplace=True)
-
     training.drop("PassengerId", axis=1, inplace=True)
-
     training.drop("Name", axis=1, inplace=True)
-
     training.drop("Ticket", axis=1, inplace=True)
-
     final_training = training
-
     print(final_training.head())
-
     final_training['IsMinor'] = np.where(final_training['Age'] <= 16, 1, 0)
-
     feature_selection(final_training)
-
-
-
     return  final_training
 
 
@@ -91,10 +62,6 @@ def preprocess_test_data():
     final_test = testing
 
     final_test['IsMinor'] = np.where(final_test['Age'] <= 16, 1, 0)
-
-    #print(final_test.head())
-
-    #feature_selection(final_test)
     return final_test
 
 def feature_selection(final_train_test):
@@ -115,50 +82,24 @@ def feature_selection(final_train_test):
 
 def data_analysis():
     train_data = loaddata.load_train_data("./data/titanic-dataset","train.csv")
-    #test_data = loaddata.load_test_data("./data/titanic-dataset","test.csv")
-    # check missing values in the data
-
     missing_Values = train_data.isna().sum()
     print(missing_Values)
 
     # missing values for a particular row
-
     missing_values_age = train_data['Age'].isna().sum()
-
     missing_values_cabin = train_data['Cabin'].isna().sum()
 
     print('missing values in the age ', missing_values_age)
-
     print ('percent of missing age in the train data ', missing_values_age/train_data.shape[0]*100)
-
     print('percent of missing cabin values in the train data' , missing_values_cabin/train_data.shape[0]*100)
 
     # embarked missing values
-
     missing_values_embarked = train_data['Embarked'].isna().sum()
-
     print('missing embarked ', missing_values_embarked)
-
     print('percentage of embarked values missing in the training data is ',missing_values_embarked/train_data.shape[0]*10)
 
     # boarded passengers grouped by the port of embarkation
 
     print('Boarded passengers grouped by the port of embarkation' , train_data['Embarked'].value_counts())
-
     # the most common port of embarkation
-
     print(' the most common port of embarkation is ', train_data['Embarked'].value_counts().idxmax())
-
-
-if __name__ == '__main__':
-    test_data = loaddata.load_test_data("./data/titanic-dataset", "test.csv")
-    data_analysis()
-    final_train = preprocess_train_data()
-    final_test= preprocess_test_data()
-
-    Selected_features = ['Age', 'TravelAlone', 'Pclass_1', 'Pclass_2', 'Embarked_C',
-                         'Embarked_S', 'Sex_male', 'IsMinor']
-    logreg = LogisticRegression()
-    train_and_evaluate(final_train,Selected_features,logreg)
-    predict_actual(final_test,Selected_features,test_data,logreg)
-
