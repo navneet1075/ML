8a2921744c2fc626fd8eb27a6faa06a56ff3351e
navneet <navneet1075@gmail.com>
2019-09-16 10:39:15 +0800

    Update naive-bayes-gauss-2.py

--- algorithms/classification/naive-bayes/naive-bayes-gauss-2.py
+++ algorithms/classification/naive-bayes/naive-bayes-gauss-2.py
@@ -8,7 +8,7 @@ print ("Features: ", wine.feature_names)
 
 print("Labels", wine.target_names)
 
-X_train, X_test, y_train, y_test = train_test_split(wine.data, wine.target, test_size=0.3,random_state=109) # 70% training and 30% test
+X_train, X_test, y_train, y_test = train_test_split(wine.data, wine.target, test_size=0.3,random_state=1) # 70% training and 30% test
 
 gnb = GaussianNB()
 
