c6f3ff150670a02327cd7f1c611b45d3a5cc07c9
navneet1075 <navneet1075@gmail.com>
2019-09-16 10:09:33 +0800

    adding notes

---
+++ algorithms/classification/naive-bayes/naive-bayes-gauss-2.py
@@ -0,0 +1,19 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics
+wine = datasets.load_wine()
+
+print ("Features: ", wine.feature_names)
+
+print("Labels", wine.target_names)
+
+X_train, X_test, y_train, y_test = train_test_split(wine.data, wine.target, test_size=0.3,random_state=109) # 70% training and 30% test
+
+gnb = GaussianNB()
+
+gnb.fit(X_train, y_train)
+y_pred = gnb.predict(X_test)
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+
--- algorithms/classification/naive-bayes/naive-bayes-multinomial.py
+++
@@ -1,6 +0,0 @@
-from sklearn import datasets
-wine = datasets.load_wine()
-
-print ("Features: ", wine.feature_names)
-
-print("Labels", wine.target_names)
